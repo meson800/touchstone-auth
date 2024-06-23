83163e252d3d08c0f92ebf4c8a4e30bfbb55998f
Christopher Johnstone <meson800@gmail.com>
2024-06-23 14:07:24 -0400

    Updated debugging functionality

--- src/touchstone_auth/__main__.py
+++ src/touchstone_auth/__main__.py
@@ -1,25 +1,25 @@
 from touchstone_auth import TouchstoneSession, UsernamePassAuth, TwofactorType
 from bs4 import BeautifulSoup  # type: ignore
 import json
+from argparse import ArgumentParser
+
+parser = ArgumentParser(prog='touchstone-auth-debugger')
+parser.add_argument('--url', default='https://atlas.mit.edu', required=False)
+parser.add_argument('--credentials', default='credentials.json', required=False)
 
 if __name__ == '__main__':
     # For debugging
+    args = parser.parse_args()
     print("Debugging session")
 
-    with open('credentials.json', encoding='utf-8') as configfile:
+    with open(args.credentials, encoding='utf-8') as configfile:
         config = json.load(configfile)
 
-    with TouchstoneSession('https://atlas.mit.edu',
+    with TouchstoneSession(args.url,
             # Username/pass auth
             auth_type=UsernamePassAuth(config['username'], config['password']), cookiejar_filename='cookiejar.pickle',
-            # Certificate auth
-            #auth_type=CertificateAuth(config['certfile'], config['password']), cookiejar_filename='cookiejar.pickle',
-            # Byte-loaded certificate auth
-            #auth_type=CertificateAuth(open(config['certfile'], 'rb').read(), config['password']), cookiejar_filename='cookiejar.pickle',
-            # Deprecated certificate auth
-            #config['certfile'], config['password'], cookiejar_filename='cookiejar.pickle',
             twofactor_type=TwofactorType.DUO_PUSH,
             verbose=True) as s:
-        r = s.get('https://atlas.mit.edu')
+        r = s.get(args.url)
         parsed_html = BeautifulSoup(r.text, features='html.parser')
-        print(parsed_html.find('title').string)
+        print(f"Title: {parsed_html.find('title').string}")
